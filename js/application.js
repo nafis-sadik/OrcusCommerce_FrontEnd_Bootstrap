bd1318691378533581af125adc548b0bea1af46e
Nafis Sadik <nafis_sadik@outlook.com>
2021-04-17 01:08:19 +0600

    search not added yet, rest of nav bar done

--- js/application.js
+++ js/application.js
@@ -1,31 +1,63 @@
 const Rounte = {
-    baseUrl: 'https://localhost:44376/',
-    BEUrl: 'https://'
-}
+  baseUrl: "http://127.0.0.1:5501/",
+  BEUrl: "https://localhost:44376/",
+};
 
 let getCategories = (shopId) => {
-    console.log(Rounte.baseUrl + 'api/Categories/' + shopId);
-    $.ajax({
-        url: Rounte.baseUrl + 'api/Categories/' + shopId,
-        success: (res) => { 
-            res.forEach(categories => {
-                if(categories.subcategories.length > 0){
-                    let dropdown = '';
-                    categories.subcategories.forEach(subcategories => {
-                        dropdown += '<li><a class="dropdown-item" href="#">'+ subcategories.subCategoryName +'</a></li>';
-                    });
-                    $('#navbar-nav').append('<li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="' + categories.categoryName  + '" role="button" data-bs-toggle="dropdown" aria-expanded="false">'  + categories.categoryName + '</a>'
-                        + '<ul class="dropdown-menu" aria-labelledby="' + categories.categoryName  + '">' + dropdown + '</ul>'
-                    +'</li>');
-                    dropdown = '';
-                }
-                else
-                    $('#navbar-nav').append('<li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="' + categories.categoryName  + '" role="button" data-bs-toggle="dropdown" aria-expanded="false">'  + categories.categoryName + '</a> </li>');
+  $.ajax({
+      url: Rounte.BEUrl + "api/Categories/" + shopId,
+      success: (res) => {
+        res.forEach((categories) => {
+          if (categories.subcategories.length > 0) {
+            let dropdown = '<div class="dropdown-menu shadow-sm sm-menu" aria-labelledby="' + categories.categoryName + '">';
+            categories.subcategories.forEach((subcategories) => {
+              dropdown += '<a class="dropdown-item" href="#">' + subcategories.subCategoryName + "</a>";
             });
-        }
-    });
+            dropdown += '</div>';
+
+            $("#navbar-nav").append('<li class="nav-item px-lg-2 dropdown d-menu ripple">' + GetDropdownFrame(categories.categoryName) + dropdown + '</li>');
+            dropdown = "";
+          } else {
+            $("#navbar-nav").append(
+              '<li class="nav-item px-lg-2 d-menu"> <a class="nav-link" href="#"><span class="d-inline-block d-lg-none icon-width"><i class="fas fa-spa"></i></span>' + categories.categoryName + '</a> </li>'
+            );
+          }
+        });
+        enableHoverNavbar();
+      },
+  });
+};
+
+let GetDropdownFrame = (DrowdownLabel) => {
+  let frame = '';
+  frame = '<a class="nav-link dropdown-toggle" href="#" id="'+DrowdownLabel+'" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">';
+  frame += '<span class="d-inline-block d-lg-none icon-width">';
+  frame += '<i class="far fa-caret-square-down"></i>';
+  frame += '</span>';
+  frame += DrowdownLabel;
+  frame += '<svg  id="arrow" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">';
+  frame += '<polyline points="6 9 12 15 18 9"></polyline>';
+  frame += '</svg>';
+  frame += '</a>';
+  return frame;
 }
 
 $(document).ready(() => {
-    getCategories(1);
-})
+  getCategories(1);
+  enableHoverNavbar();
+});
+
+let enableHoverNavbar = () => {
+  if ($(window).width() > 991){
+    $('.navbar .d-menu').hover(
+      function () {
+        $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+      }, function () {
+        $(this).find('.sm-menu').first().stop(true, true).delay(120).slideUp(100);
+      });
+  }
+}
+
+// Reskinning Guidance
+// Classes not to be removed
+// navbar, d-menu
